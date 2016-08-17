5dff88035e2b046ef4d16116df12a1170d1351ef
Norman Lorrain <normanlorrain@gmail.com>
2016-08-17 06:47:04 -0600

    finds the ripper;..  Metadata search

--- metadata.py
+++ metadata.py
@@ -1,2 +1,8 @@
 artist = "Ken Follett"
-title = "Eye of the Needle 2"
+title = "Eye of the Needle"
+
+
+if __name__ == '__main__':
+    import webbrowser
+    url = 'https://www.google.com.tr/search?q="{}+{}+audiobook'.format(artist.replace(' ','+'), title.replace(' ','+'))    
+    webbrowser.open(url)
